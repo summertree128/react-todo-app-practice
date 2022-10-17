f9c5491910893f555590d13a135619df33fd0aa2
summertree128 <natsukinoue530000@gmail.com>
2022-10-18 01:24:22 +0900

    Add TodoItem component with delete feature

--- todo.js
+++ todo.js
@@ -4,13 +4,17 @@ class TodoApp extends React.Component {
     this.state = { items: [], text: '' };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this)
   }
 
   render() {
     return (
       <div>
         <h3>TODO</h3>
-        <TodoList items={this.state.items} />
+        <TodoList
+          items={this.state.items}
+          onDelete={this.handleDelete}
+        />
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="new-todo">
             What needs to be done?
@@ -46,17 +50,58 @@ class TodoApp extends React.Component {
       text: ''
     }));
   }
+
+  handleDelete (itemId) {
+    this.setState(state => ({
+      items: state.items.filter(item => item.id != itemId)
+    }))
+  }
 }
 
 class TodoList extends React.Component {
+  constructor (props) {
+    super(props)
+    this.handleDelete = this.handleDelete.bind(this)
+  }
+
   render() {
     return (
       <ul>
         {this.props.items.map(item => (
-          <li key={item.id}>{item.text}</li>
+          <li key={item.id}>
+            <TodoItem
+              item={item}
+              onDelete={this.handleDelete}
+            />
+          </li>
         ))}
       </ul>
     );
+  }
+
+  handleDelete (itemId) {
+    this.props.onDelete(itemId)
+  }
+}
+
+class TodoItem extends React.Component {
+  constructor (props) {
+    super(props)
+    this.handleDelete = this.handleDelete.bind(this)
+  }
+
+  render () {
+    return (
+      <React.Fragment>
+        {this.props.item.text}
+        <button onClick={this.handleDelete} value={this.props.item.id}>Delete</button>
+      </React.Fragment>
+    )
+  }
+
+  handleDelete (e) {
+    e.preventDefault()
+    this.props.onDelete(e.target.value)
   }
 }
 
