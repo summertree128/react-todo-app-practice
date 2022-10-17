2c547b8fdef154e6d291dedfe7ae1c4d17b3b0c6
summertree128 <natsukinoue530000@gmail.com>
2022-10-18 02:26:59 +0900

    Implement update feature

--- todo.js
+++ todo.js
@@ -5,6 +5,7 @@ class TodoApp extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleDelete = this.handleDelete.bind(this)
+    this.handleUpdate = this.handleUpdate.bind(this)
   }
 
   render() {
@@ -14,6 +15,7 @@ class TodoApp extends React.Component {
         <TodoList
           items={this.state.items}
           onDelete={this.handleDelete}
+          onUpdate={this.handleUpdate}
         />
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="new-todo">
@@ -56,12 +58,20 @@ class TodoApp extends React.Component {
       items: state.items.filter(item => item.id != itemId)
     }))
   }
+
+  handleUpdate (itemId, text) {
+    const newItems = [...this.state.items]
+    const editedItem = newItems.find(item => item.id == itemId)
+    editedItem.text = text
+    this.setState({ items: newItems })
+  }
 }
 
 class TodoList extends React.Component {
   constructor (props) {
     super(props)
     this.handleDelete = this.handleDelete.bind(this)
+    this.handleUpdate = this.handleUpdate.bind(this)
   }
 
   render() {
@@ -72,6 +82,7 @@ class TodoList extends React.Component {
             <TodoItem
               item={item}
               onDelete={this.handleDelete}
+              onUpdate={this.handleUpdate}
             />
           </li>
         ))}
@@ -82,18 +93,39 @@ class TodoList extends React.Component {
   handleDelete (itemId) {
     this.props.onDelete(itemId)
   }
+
+  handleUpdate (itemId, text) {
+    this.props.onUpdate(itemId, text)
+  }
 }
 
 class TodoItem extends React.Component {
   constructor (props) {
     super(props)
-    this.handleDelete = this.handleDelete.bind(this)
+    this.state = { editing: false, text: this.props.item.text };
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   render () {
+    if (this.state.editing) {
+      return (
+        <React.Fragment>
+          <form onSubmit={this.handleUpdate}>
+            <input type='text' name='text' value={this.state.text} onChange={this.handleChange} />
+            <input type='hidden' name='id' value={this.props.item.id} />
+            <button>Save</button>
+          </form>
+        </React.Fragment>
+      )
+    }
+
     return (
       <React.Fragment>
         {this.props.item.text}
+        <button onClick={this.handleEdit} value={this.props.item.id}>Edit</button>
         <button onClick={this.handleDelete} value={this.props.item.id}>Delete</button>
       </React.Fragment>
     )
@@ -102,6 +134,21 @@ class TodoItem extends React.Component {
   handleDelete (e) {
     e.preventDefault()
     this.props.onDelete(e.target.value)
+  }
+
+  handleEdit(e) {
+    e.preventDefault();
+    this.setState({ editing: true })
+  }
+
+  handleUpdate(e) {
+    e.preventDefault();
+    this.props.onUpdate(this.props.item.id, this.state.text)
+    this.setState({ editing: false })
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value })
   }
 }
 
